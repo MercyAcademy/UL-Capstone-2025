ebc36f10548c214528c68985a7c5ed8662e444c4
Carsten Kurtz <ctkurt02@louisville.edu>
2025-02-06 17:40:43 -0500

    Update VerkadaOperations.py

--- VerkadaOperations.py
+++ VerkadaOperations.py
@@ -2,6 +2,8 @@ import requests
 import os
 from dotenv import load_dotenv
 
+token = ""
+
 def get_api_token():
     load_dotenv()
 
@@ -16,5 +18,24 @@ def get_api_token():
     token = r.json()['token']
     return token
     
+def getdoorids():
+    url = "https://api.verkada.com/access/v1/doors"
 
-get_api_token()
+    headers = {
+    "accept": "application/json",
+    "x-verkada-auth": token
+    }
+
+    response = requests.get(url, headers=headers)
+    response = response.json()
+
+    doorids = []
+    i = 0
+    for key in response["doors"]:
+        doorids.append(response["doors"][i]["door_id"])
+        i +=1
+        ###this may be trivially solvable with a for loop
+    return(doorids)
+
+token = get_api_token()
+print(getdoorids())
