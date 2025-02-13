782eb9026bd43b8739b3ba0f2474dab1742e818f
Carsten Kurtz <ctkurt02@louisville.edu>
2025-02-13 14:34:59 -0500

    proper format and logging the cwd because its deciding to save strange places

--- configcreator.py
+++ configcreator.py
@@ -1,30 +1,26 @@
 import configparser
-
+import os
+import logging 
 
 def create_config():
     config = configparser.ConfigParser()
 
-    # Add sections and key-value pairs
     config['General'] = {'testmode': False, 'Days_To_Schedule': 10}
     config['Google'] = {'groups': 'group_names'}
 
-    # Write the configuration to a file
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+    logging.log(os.getcwd())
+    
 def read_config():
-    # Create a ConfigParser object
     config = configparser.ConfigParser()
-
-    # Read the configuration file
     config.read('config.ini')
 
-    # Access values from the configuration file
     debug_mode = config.getboolean('General', 'testmode')
     log_level = config.getint('General', 'Days_To_Schedule')
     db_name = config.get('Google', 'groups')
 
-    # Return a dictionary with the retrieved values
     config_values = {
         'testmode': debug_mode,
         'Days_To_Schedule': log_level,
