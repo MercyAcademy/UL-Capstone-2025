c4493feff53cd46bff40b5341bc6d5b69c935c39
Carsten Kurtz <ctkurt02@louisville.edu>
2025-04-03 12:22:20 -0400

    Multithreading and email image in config
    Added multithreading to the email so it starts on a child process instead of locking the cpu while it waits for an api response. Also added a config value for the path to the image thats included in the email.

--- configcreator.py
+++ configcreator.py
@@ -6,7 +6,7 @@ def create_config():
     config = configparser.ConfigParser()
 
     config['General'] = {'testmode': False, 'Days_To_Schedule': 10,'Send_Emails': True,'Setting 4': False,'Setting 5': False}
-    config['Google'] = {'groups': 'group_names','mailinglist': 'emails'}
+    config['Google'] = {'groups': 'group_names','mailinglist': 'emails','emailimagepath': 'emails'}
 
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
@@ -19,16 +19,18 @@ def read_config():
 
     debug_mode = config.getboolean('General', 'testmode')
     log_level = config.getint('General', 'Days_To_Schedule')
-    Send_Emails = config.getboolean('General', 'Send_Emails')
+    Send_Emails = config.getboolean('General', 'send_emails')
 
     db_name = config.get('Google', 'groups')
     mailinglist = config.get('Google','mailinglist')
+    emailimagepath = config.get('Google','emailimagepath')
     config_values = {
         'testmode': debug_mode,
         'Days_To_Schedule': log_level,
         'Send_Emails': Send_Emails,
         'groups': db_name,
-        'mailinglist': mailinglist
+        'mailinglist': mailinglist,
+        'emailimagepath': emailimagepath
     }
 
     return config_values
