2850758cf4fb95b9471085a20c6a11519f432407
Kushal Khatiwada <kushalkhatiwada03@gmail.com>
2025-04-10 13:56:46 -0400

    Updated requirements.txt, deleted testscript.py and requirements
    Signed-off-by: Kushal Khatiwada <kushalkhatiwada03@gmail.com>

--- TestScript.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-log_file = "test_output.txt"
-
-now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-with open(log_file, "a") as file:
-    file.write(f"Test script executed at: {now}\n")
